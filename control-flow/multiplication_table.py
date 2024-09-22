214be82bc307f0e0e2c1640218ea070fca3d1d3a
Repher Akello <akellorepher@gmail.com>
2024-09-22 21:48:14 +0300

    making changes on the print

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,7 @@
 number = eval(input("Enter a number to see its multiplication table:"))
 
 
-for i in range(11):
+for i in range(1,11):
     product = number * i
 
     print(f"{number} * {i} = {product}")
