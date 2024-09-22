9ec9f91a7712ab7ee49859eea791428db68be73b
Repher Akello <akellorepher@gmail.com>
2024-09-22 20:27:27 +0300

    adding new directory control-flow

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+weather = str(input("What's the weather like today? (sunny/rainy/cold):"))
+
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+if weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+if weather == "cold":
+    print("Make sure to wear a warm coat and a scarf")
+else:
+    print("Sorry, i don't have recommendations for this weather.")
+    
