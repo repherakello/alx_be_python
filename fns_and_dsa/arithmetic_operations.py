bb8c61ea54bd62f73620bafbda1561650cec782e
Repher Akello <akellorepher@gmail.com>
2024-10-04 11:07:08 +0300

    adding the changes on arithmetic file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,18 @@
 def perform_operation(num1:float, num2:float,operation:str):
+    if operation not in ["add", "subtract","multiply","divide"]:
+        return "Error Invalid operation"
+    
     if operation == "add":
         return num1 + num2
-    elif operation == "subtract":
+    elif operation =="subtract":
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
         if num2 == 0:
-            return "Error: Division by zero!"
+            return "Error: Division by zero"
         return num1 / num2
-    else:
-        return "Error: invalid operation"
-     
+    
+
+result = perform_operation(10,5,"subtract")
+print(result)
